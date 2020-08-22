97c09ed348cf0ca54fce37435bcedb5b9ea4b543
Nick Baughan <nicholas.baughan@hotmail.co.uk>
2020-08-22 09:01:40 +0100

    Explored looking at CloudKit

--- Chronoderm/AppDelegate.swift
+++ Chronoderm/AppDelegate.swift
@@ -136,13 +136,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data stack
     
     lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-         */
-            let container = NSPersistentContainer(name: "Chronoderm")
+        
+            let container = NSPersistentCloudKitContainer(name: "Chronoderm")
+        
+        // Create a store description for a local store
+           let localStoreLocation = URL(fileURLWithPath: "/path/to/local.store")
+           let localStoreDescription =
+               NSPersistentStoreDescription(url: localStoreLocation)
+           localStoreDescription.configuration = "Local"
+           
+           // Create a store description for a CloudKit-backed local store
+           let cloudStoreLocation = URL(fileURLWithPath: "/path/to/cloud.store")
+           let cloudStoreDescription =
+               NSPersistentStoreDescription(url: cloudStoreLocation)
+           cloudStoreDescription.configuration = "Cloud"
+
+           // Set the container options on the cloud store
+           cloudStoreDescription.cloudKitContainerOptions =
+               NSPersistentCloudKitContainerOptions(
+                   containerIdentifier: "com.iCloud.baughan.chronoderm.chronoderm")
+           
+           // Update the container's list of store descriptions
+           container.persistentStoreDescriptions = [
+               cloudStoreDescription,
+               localStoreDescription
+           ]
         container.loadPersistentStores { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
